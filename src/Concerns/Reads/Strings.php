92be0c331d96041e8bdfe0c698e6f20f65a25a07
Brian Faust <envoyer@pm.me>
2018-07-18 15:56:46 +0300

    implement reade & write for string
    Signed-off-by: Brian Faust <envoyer@pm.me>

--- src/Concerns/Reads/Strings.php
+++ src/Concerns/Reads/Strings.php
@@ -6,26 +6,26 @@ trait Strings
 {
     public function readCString(int $offset = 0): self
     {
-        //
+        return $this;
     }
 
     public function readIString(int $offset = 0): self
     {
-        //
+        return $this;
     }
 
     public function readString(int $length, int $offset = 0): self
     {
-        //
+        return $this;
     }
 
     public function readUTF8String(int $length, int $offset = 0): self
     {
-        //
+        return $this;
     }
 
     public function readVString(int $offset = 0): self
     {
-        //
+        return $this;
     }
 }
--- src/Concerns/Writes/Strings.php
+++ src/Concerns/Writes/Strings.php
@@ -4,28 +4,30 @@ namespace BrianFaust\ByteBuffer\Concerns\Writes;
 
 trait Strings
 {
-    public function writeBytes(string $value, int $offset = 0): self
+    public function writeBytes($value, int $offset = 0): self
     {
-        //
+        return $this->append($value, $offset);
     }
 
-    public function writeCString(string $str, int $offset = 0): self
+    public function writeCString(string $value, int $offset = 0): self
     {
-        //
+        return $this;
     }
 
-    public function writeIString(string $str, int $offset = 0): self
+    public function writeIString(string $value, int $offset = 0): self
     {
-        //
+        return $this;
     }
 
     public function writeString(string $value, int $offset = 0): self
     {
-        //
+        $length = strlen($value);
+
+        return $this->pack("a{$length}", $value, $offset, $length);
     }
 
-    public function writeVString(string $str, int $offset = 0): self
+    public function writeVString(string $value, int $offset = 0): self
     {
-        //
+        return $this;
     }
 }
