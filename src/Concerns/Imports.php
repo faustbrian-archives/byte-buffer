cc92ebe6c2e37586a0dfae3c51631174034d352b
Brian Faust <envoyer@pm.me>
2018-07-18 15:57:23 +0300

    implement import methods
    Signed-off-by: Brian Faust <envoyer@pm.me>

--- src/Concerns/Imports.php
+++ src/Concerns/Imports.php
@@ -6,21 +6,21 @@ trait Imports
 {
     public static function fromBase64(string $value): self
     {
-        //
+        return new static(base64_decode($value, true));
     }
 
     public static function fromBinary(string $value): self
     {
-        //
+        return new static($value);
     }
 
     public static function fromHex(string $value): self
     {
-        //
+        return new static(hex2bin($value));
     }
 
     public static function fromUTF8(string $value): self
     {
-        //
+        return new static(mb_convert_encoding($value, 'UTF-16', 'UTF-8'));
     }
 }
