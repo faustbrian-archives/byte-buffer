e0dcb0664ef62903f15cb29d851366e7d72a77f9
Brian Faust <envoyer@pm.me>
2018-07-18 15:56:53 +0300

    implement reade & write for hex
    Signed-off-by: Brian Faust <envoyer@pm.me>

--- src/Concerns/Reads/Hex.php
+++ src/Concerns/Reads/Hex.php
@@ -6,16 +6,18 @@ trait Hex
 {
     public function readHex(int $length): string
     {
-        //
+        $length *= 2;
+
+        return $this->unpack("H{$length}", $length / 2);
     }
 
     public function readHexBytes(int $length): string
     {
-        //
+        return hex2bin($this->readHexRaw($length));
     }
 
     public function readHexRaw(int $length): string
     {
-        //
+        return substr($this->hex, $this->offset, $length);
     }
 }
--- src/Concerns/Writes/Hex.php
+++ src/Concerns/Writes/Hex.php
@@ -6,6 +6,8 @@ trait Hex
 {
     public function writeHex(string $value, int $offset = 0): self
     {
-        //
+        $length = strlen($value);
+
+        return $this->pack("H{$length}", $value, $offset, $length);
     }
 }
