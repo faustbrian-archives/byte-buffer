eecaa14c849c4eabe72c166af1a45a7be054e02d
Brian Faust <envoyer@pm.me>
2018-07-18 15:56:05 +0300

    implement init, pack and unpack
    Signed-off-by: Brian Faust <envoyer@pm.me>

--- src/ByteBuffer.php
+++ src/ByteBuffer.php
@@ -3,6 +3,7 @@
 namespace BrianFaust\ByteBuffer;
 
 use SplFixedArray;
+use InvalidArgumentException;
 
 class ByteBuffer
 {
@@ -28,9 +29,15 @@ class ByteBuffer
     private $offset     = 0;
     private $endianness = 1;
 
-    public function __construct($argument)
+    public function __construct($value)
     {
-        //
+        if (is_string($value)) {
+            $this->initializeBuffer(strlen($value), $value);
+        } elseif (is_int($value)) {
+            $this->initializeBuffer($value, pack("x{$value}"));
+        } else {
+            throw new InvalidArgumentException('Constructor argument must be a binary string or integer.');
+        }
     }
 
     public static function allocate(int $capacity): self
@@ -148,21 +155,39 @@ class ByteBuffer
 
     protected function initializeBuffer(int $length, $content): void
     {
-        //
+        $this->buffer = new SplFixedArray($length);
+
+        for ($i = 0; $i < $length; ++$i) {
+            $this->buffer[$i] = $content[$i];
+        }
+
+        $this->limit = $length;
     }
 
     protected function pack(string $format, $value, int $offset): self
     {
-        //
+        $bytes = pack($format, $value);
+
+        for ($i = 0; $i < strlen($bytes); ++$i) {
+            $this->buffer[$this->offset++] = $bytes[$i];
+        }
+
+        return $this;
     }
 
     protected function unpack(string $format, int $offset = 0)
     {
-        //
+        $value = unpack($format, $this->toBinary(), $this->offset)[1];
+
+        $this->skip($offset ?: LengthMap::get($format));
+
+        return $value;
     }
 
-    protected function checkForOverSize(int $expected, int $actual): void
+    protected function exceeds($expected, int $actual): void
     {
-        //
+        if ($actual > $expected) {
+            throw new InvalidArgumentException(sprintf('%d exceeded limit of %d', $actual, $expected));
+        }
     }
 }
