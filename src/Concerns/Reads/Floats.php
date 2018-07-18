9e00ad0bf8892332e3245299c48fbdc0060521a9
Brian Faust <envoyer@pm.me>
2018-07-18 15:56:59 +0300

    implement reade & write for floats
    Signed-off-by: Brian Faust <envoyer@pm.me>

--- src/Concerns/Reads/Floats.php
+++ src/Concerns/Reads/Floats.php
@@ -6,11 +6,11 @@ trait Floats
 {
     public function readFloat(int $offset = 0): float
     {
-        //
+        return $this->unpack(['G', 'g', 'f'][$this->endianness], $offset);
     }
 
     public function readDouble(int $offset = 0): float
     {
-        //
+        return $this->unpack(['E', 'e', 'd'][$this->endianness], $offset);
     }
 }
--- src/Concerns/Writes/Floats.php
+++ src/Concerns/Writes/Floats.php
@@ -6,11 +6,11 @@ trait Floats
 {
     public function writeFloat(float $value, int $offset = 0): self
     {
-        //
+        return $this->pack(['G', 'g', 'f'][$this->endianness], $value, $offset);
     }
 
     public function writeDouble(float $value, int $offset = 0): self
     {
-        //
+        return $this->pack(['E', 'e', 'd'][$this->endianness], $value, $offset);
     }
 }
