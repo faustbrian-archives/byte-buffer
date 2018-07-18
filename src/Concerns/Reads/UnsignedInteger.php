3473242986d948c9ce2178a803d8f4ea5fc3887e
Brian Faust <envoyer@pm.me>
2018-07-18 15:56:29 +0300

    implement reade & write for unsigned integer
    Signed-off-by: Brian Faust <envoyer@pm.me>

--- src/Concerns/Reads/UnsignedInteger.php
+++ src/Concerns/Reads/UnsignedInteger.php
@@ -6,41 +6,41 @@ trait UnsignedInteger
 {
     public function readUInt8(int $offset = 0): int
     {
-        //
+        return $this->unpack('C', $offset);
     }
 
     public function readUInt16(int $offset = 0): int
     {
-        //
+        return $this->unpack(['n', 'v', 'S'][$this->endianness], $offset);
     }
 
     public function readUInt32(int $offset = 0): int
     {
-        //
+        return $this->unpack(['N', 'V', 'L'][$this->endianness], $offset);
     }
 
     public function readUInt64(int $offset = 0): int
     {
-        //
+        return $this->unpack(['J', 'P', 'Q'][$this->endianness], $offset);
     }
 
     public function readUByte(int $offset = 0): self
     {
-        //
+        return $this->readUInt8($offset);
     }
 
     public function readUShort(int $offset = 0): self
     {
-        //
+        return $this->readUInt16($offset);
     }
 
     public function readUInt(int $offset = 0): self
     {
-        //
+        return $this->readUInt32($offset);
     }
 
     public function readULong(int $offset = 0): self
     {
-        //
+        return $this->readUInt64($offset);
     }
 }
--- src/Concerns/Writes/UnsignedInteger.php
+++ src/Concerns/Writes/UnsignedInteger.php
@@ -6,41 +6,57 @@ trait UnsignedInteger
 {
     public function writeUInt8(int $value, int $offset = 0): self
     {
-        //
+        $format = 'C';
+
+        $this->checkForOverSize(0xff, $value);
+
+        return $this->pack($format, $value, $offset);
     }
 
     public function writeUInt16(int $value, int $offset = 0): self
     {
-        //
+        $format = ['n', 'v', 'S'][$this->endianness];
+
+        $this->checkForOverSize(0xffff, $value);
+
+        return $this->pack($format, $value, $offset);
     }
 
     public function writeUInt32(int $value, int $offset = 0): self
     {
-        //
+        $format = ['N', 'V', 'L'][$this->endianness];
+
+        $this->checkForOverSize(0xffffffff, $value);
+
+        return $this->pack($format, $value, $offset);
     }
 
     public function writeUInt64(int $value, int $offset = 0): self
     {
-        //
+        $format = ['J', 'P', 'Q'][$this->endianness];
+
+        $this->checkForOverSize(0xffffffffffffffff, $value);
+
+        return $this->pack($format, $value, $offset);
     }
 
     public function writeUByte(int $value, int $offset = 0): self
     {
-        //
+        return $this->writeUInt8($value, $offset);
     }
 
     public function writeUShort(int $value, int $offset = 0): self
     {
-        //
+        return $this->writeUInt16($value, $offset);
     }
 
     public function writeUInt(int $value, int $offset = 0): self
     {
-        //
+        return $this->writeUInt32($value, $offset);
     }
 
     public function writeULong(int $value, int $offset = 0): self
     {
-        //
+        return $this->writeUInt64($value, $offset);
     }
 }
