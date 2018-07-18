e489f676665e0ce23e023df9b3f7ee971e615392
Brian Faust <envoyer@pm.me>
2018-07-18 15:56:38 +0300

    implement reade & write for integer
    Signed-off-by: Brian Faust <envoyer@pm.me>

--- src/Concerns/Reads/Integer.php
+++ src/Concerns/Reads/Integer.php
@@ -6,41 +6,41 @@ trait Integer
 {
     public function readInt8(int $offset = 0): self
     {
-        //
+        return $this->unpack('c', $offset);
     }
 
     public function readInt16(int $offset = 0): self
     {
-        //
+        return $this->unpack('s', $offset);
     }
 
     public function readInt32(int $offset = 0): self
     {
-        //
+        return $this->unpack('l', $offset);
     }
 
     public function readInt64(int $offset = 0): self
     {
-        //
+        return $this->unpack('q', $offset);
     }
 
     public function readByte(int $offset = 0): self
     {
-        //
+        return $this->readInt8($offset);
     }
 
     public function readShort(int $offset = 0): self
     {
-        //
+        return $this->readInt16($offset);
     }
 
     public function readInt(int $offset = 0): self
     {
-        //
+        return $this->readInt32($offset);
     }
 
     public function readLong(int $offset = 0): self
     {
-        //
+        return $this->readInt64($offset);
     }
 }
--- src/Concerns/Writes/Integer.php
+++ src/Concerns/Writes/Integer.php
@@ -6,41 +6,41 @@ trait Integer
 {
     public function writeInt8(int $value, int $offset = 0): self
     {
-        //
+        return $this->pack('c', $value, $offset);
     }
 
     public function writeInt16(int $value, int $offset = 0): self
     {
-        //
+        return $this->pack('s', $value, $offset);
     }
 
     public function writeInt32(int $value, int $offset = 0): self
     {
-        //
+        return $this->pack('l', $value, $offset);
     }
 
     public function writeInt64(int $value, int $offset = 0): self
     {
-        //
+        return $this->pack('q', $value, $offset);
     }
 
     public function writeByte(int $value, int $offset = 0): self
     {
-        //
+        return $this->writeInt8($value, $offset);
     }
 
     public function writeShort(int $value, int $offset = 0): self
     {
-        //
+        return $this->writeInt16($value, $offset);
     }
 
     public function writeInt(int $value, int $offset = 0): self
     {
-        //
+        return $this->writeInt32($value, $offset);
     }
 
     public function writeLong(int $value, int $offset = 0): self
     {
-        //
+        return $this->writeInt64($value, $offset);
     }
 }
