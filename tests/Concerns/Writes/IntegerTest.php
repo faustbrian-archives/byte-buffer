1aaad46f57a85c7e97bf5f317b45c79113a13a73
Brian Faust <envoyer@pm.me>
2018-07-20 07:11:45 +0300

    add writer IntegerTest
    Signed-off-by: Brian Faust <envoyer@pm.me>

---
+++ tests/Concerns/Writes/IntegerTest.php
@@ -0,0 +1,98 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of ByteBuffer.
+ *
+ * (c) Brian Faust <envoyer@pm.me>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace BrianFaust\Tests\ByteBuffer\Concerns\Writes;
+
+use BrianFaust\ByteBuffer\ByteBuffer;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * This is the integer writer test class.
+ *
+ * @author Brian Faust <envoyer@pm.me>
+ * @covers \BrianFaust\ByteBuffer\Concerns\Writes\Integer
+ */
+class IntegerTest extends TestCase
+{
+    /** @test */
+    public function it_should_write_int8()
+    {
+        $buffer = ByteBuffer::new(1);
+        $buffer->writeInt8(8);
+
+        $this->assertSame(1, $buffer->capacity());
+    }
+
+    /** @test */
+    public function it_should_write_int16()
+    {
+        $buffer = ByteBuffer::new(1);
+        $buffer->writeInt16(16);
+
+        $this->assertSame(2, $buffer->capacity());
+    }
+
+    /** @test */
+    public function it_should_write_int32()
+    {
+        $buffer = ByteBuffer::new(1);
+        $buffer->writeInt32(32);
+
+        $this->assertSame(4, $buffer->capacity());
+    }
+
+    /** @test */
+    public function it_should_write_int64()
+    {
+        $buffer = ByteBuffer::new(1);
+        $buffer->writeInt64(64);
+
+        $this->assertSame(8, $buffer->capacity());
+    }
+
+    /** @test */
+    public function it_should_write_byte()
+    {
+        $buffer = ByteBuffer::new(1);
+        $buffer->writeByte(8);
+
+        $this->assertSame(1, $buffer->capacity());
+    }
+
+    /** @test */
+    public function it_should_write_short()
+    {
+        $buffer = ByteBuffer::new(1);
+        $buffer->writeShort(16);
+
+        $this->assertSame(2, $buffer->capacity());
+    }
+
+    /** @test */
+    public function it_should_write_int()
+    {
+        $buffer = ByteBuffer::new(1);
+        $buffer->writeInt(32);
+
+        $this->assertSame(4, $buffer->capacity());
+    }
+
+    /** @test */
+    public function it_should_write_long()
+    {
+        $buffer = ByteBuffer::new(1);
+        $buffer->writeLong(64);
+
+        $this->assertSame(8, $buffer->capacity());
+    }
+}
