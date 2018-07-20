c631e114538054f940208b1974b2701deef52a9b
Brian Faust <envoyer@pm.me>
2018-07-20 08:47:18 +0300

    add writes HexTest
    Signed-off-by: Brian Faust <envoyer@pm.me>

---
+++ tests/Concerns/Writes/HexTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of ByteBuffer.
+ *
+ * (c) Brian Faust <envoyer@pm.me>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace BrianFaust\Tests\ByteBuffer\Concerns\Writes;
+
+use BrianFaust\ByteBuffer\ByteBuffer;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * This is the hex writer test class.
+ *
+ * @author Brian Faust <envoyer@pm.me>
+ * @covers \BrianFaust\ByteBuffer\Concerns\Writes\Hex
+ */
+class HexTest extends TestCase
+{
+    /** @test */
+    public function it_should_write_hex()
+    {
+        $buffer = ByteBuffer::new(0);
+        $buffer->writeBytes('48656c6c6f20576f726c64');
+
+        $this->assertSame(22, $buffer->capacity());
+        $this->assertSame('Hello World', $buffer->readHexString(44));
+    }
+}
